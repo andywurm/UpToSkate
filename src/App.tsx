51a6195f95e20aee1a9a0f25b64d9b5b9a75f479
andywurm <andaiyewurm@gmail.com>
2022-09-01 21:57:45 -0400

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation';
-<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
+import Skaters from './Pages/Skaters';
+import Events from './Pages/Events';
+import Results from './Pages/Results';
+import Media from './Pages/Media';
+import Home from './Pages/Home';
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 function App() {
 
   const [navColor, setNavColor] = useState<boolean>(true)
 
   return (
-    <div className="App">
-      <div style={{
-      height: "100vh",
-      width: "100vw"
-    }}>
-      <Navigation/>
+    <Router>
+      <div className="App">
+        <div style={{
+          height: "100vh",
+          width: "100vw"
+        }}>
+
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Skaters" element={<Skaters/>} />
+            <Route path="/Events" element={<Events/>}/>
+            <Route path="/Results" element={<Results/>}/>
+            <Route path="/Media" element={<Media/>}/>
+          </Routes>
+
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/Skaters.tsx
@@ -0,0 +1,6 @@
+const Skaters = () => {
+    return(
+        <div>Skaters</div>
+    )
+}
+export default Skaters;
