c675325095a896c3f1a171c5042300feb2a84904
andywurm <andaiyewurm@gmail.com>
2022-10-30 18:04:45 -0400

    Added CSS Files

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,111 +1,52 @@
 import HomePic from '../Images/HomePic.jpg'
+import '../Pages/PageCSS/HomeStyles.css'
 import { motion } from 'framer-motion'
 
-interface IPropsHome{
-    navColor : boolean
-    setNavColor : Function
+interface IPropsHome {
+    navColor: boolean
+    setNavColor: Function
 }
 
-const Home = (props : IPropsHome) => {
+const Home = (props: IPropsHome) => {
+
     props.setNavColor(false)
+    
     return (
         <motion.div
-        
+
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
 
-            style={{
-                // border: "solid red 1px",
-                height: "85%"
-            }}>
+            style={{ height: "85%" }}>
 
-            <div style={{ display: "flex", flexDirection: "row", height: "20%" }}> </div>
+            <div className='empty'> </div>
 
-            <div style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                // border: "solid red 1px",
-            }}>
+            <div className='outside'>
 
-                <div style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    textAlign: 'left',
-                    padding: "0 20px"
-                }}>
-                    <h1 style={{
-                        // border: "solid red 1px",
-                        fontWeight: "bolder",
-                        fontSize: '2.8em',
-                    }}>
+                <div className='keep'>
+                    
+                    <h1 className='text2'>
                         Keep up with all the latest <br />
                         Figure Skating News <br />
                         <span style={{ color: "#2CA9EF" }}>Now all in one place</span>
                     </h1>
-                    <h3 style={{
-                        color: "gray",
-                        fontWeight: "normal",
-                        position: 'relative',
-                        top: "-30px"
-                    }}>
-
+                    <h3 className='text1'>
                         Up to skate always has the up to date information on skaters<br />
-                        and competitions. Sign up for our newsletter.</h3>
+                        and competitions. Sign up for our newsletter.
+                    </h3>
 
-                    <div style={{
-                        display: "flex",
-                        flexDirection: 'row',
-                        position: "relative",
-                        top: "-20px"
-                    }}>
-
-                        <div style={{
-                            height: "36px",
-                            width: "70%",
-                            borderRadius: "20px",
-                            marginRight: "10px",
-                            border: "solid #2CA9EF 1px",
-                        }}>
-                            <input className='enterEmail' placeholder='Type email here...'
-                                style={{
-                                    height: "32px",
-                                    width: "70%",
-                                    marginLeft: "20px",
-                                    border: "none",
-                                    fontSize: "1.1em"
-                                }}>
-                            </input>
+                    <div className='emailBtn'>
+                        <div className='plsEmail'>
+                            <input className='enterEmail' placeholder='Type email here...' />
                         </div>
-
-                        <button className="homeBtn" style={{
-                            width: "fit-content",
-                            padding: "11px",
-                            borderRadius: "20px",
-                            backgroundColor: "#2CA9EF",
-                            border: "none",
-                            color: "white",
-                            fontWeight: "bold",
-                            fontSize: "1.1em",
-                            position: "relative",
-                            top: "-3px"
-                        }}
-                        >Sign Up</button>
-
+                        <button className="homeBtn">Sign Up</button>
                     </div>
 
                 </div>
 
                 <div style={{ display: "flex", flexDirection: "column", }}>
-                    <img style={{
-                        height: "280px",
-                        width: "300px",
-                        borderRadius: "50px",
-                        padding: "20px",
-                        position: "relative",
-                        top: "20px"
-                    }} src={HomePic} alt="skates" />
+                    <img className='skateImg' src={HomePic} alt="skates" />
                 </div>
 
             </div>
--- src/Pages/Skaters.tsx
+++ src/Pages/Skaters.tsx
@@ -2,18 +2,23 @@ import { motion } from "framer-motion"
 import search from '../Images/search.png'
 import skaters from '../data/SkaterBase'
 import DisplaySkaters from '../Components/DisplaySkaters'
+import  '../Pages/PageCSS/SkatersStyles.css'
 import { useState } from "react"
 
 interface IPropsSkaters {
     navColor: boolean
     setNavColor: Function
 }
+
 const Skaters = (props: IPropsSkaters) => {
+
     const [skaterList, setSkaterList] = useState(skaters)
+    const [clicked, setClicked] = useState("");
     const [searched, setSearched] = useState("")
     props.setNavColor(true)
 
     function filterCat(category : string){
+        setClicked(category)
         setSkaterList(skaters.filter((sk8r) => sk8r.category === category))
     }
 
@@ -28,69 +33,37 @@ const Skaters = (props: IPropsSkaters) => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
-            <div className="Search Area" style={{height: "400px",
-                backgroundColor: '#5ea6eb',
-                position: 'relative',}}>
+            <div className="SearchArea">
 
-                <div style={{ paddingTop: '100px', fontSize: '2.5em', fontWeight: '600', color: 'white' }}>Search for a Skater</div>
-                <div style={{ paddingTop: '10px', color: '#d3eaf2', fontWeight: '500', marginBottom: '3%' }}>Try typing in a name to view their information</div>
+                <div className="Search">Search for a Skater</div>
+                <div className="Type">Try typing in a name to view their information</div>
 
-                <div style={{
-                    // border: 'solid red 1px',
-                    backgroundColor: 'white',
-                    borderRadius: '20px',
-                    height: '10%',
-                    width: '45%',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    margin: '0 auto'
-                }}>
-                    <div style={{
-                        flexDirection: 'column', flexBasis: '10%',
-                        // border: 'solid pink 1px' 
-                    }}>
-                       <img onClick={() => goSearch()} src={search} style={{ height: '70%', marginTop: '5px' }} alt="" /> 
+                <div className="actualSearchArea">
+                    <div className="first">
+                       <img className="magnify" onClick={() => goSearch()} src={search} alt="" /> 
                     </div>
-                    <div style={{
-                        flexDirection: 'column', flexBasis: '90%',
-                        // border: 'solid pink 1px'
-                    }}>
-                        <input onChange={(e) => setSearched(e.target.value)} placeholder=' Type Here...' style={{ width: '97%', height: '95%', borderStyle: 'none', borderRadius: '15px', fontSize: '1.2em', }} />
+                    <div className="second">
+                        <input className="inputBar" onChange={(e) => setSearched(e.target.value)} placeholder=' Type Here...' />
                     </div>
                 </div>
 
             </div>
 
-            <div className="Display Area" style={{
-                display: 'flex',
-                flexDirection: 'row',
-                // border: 'solid red 1px',
-                width: '95%',
-                marginTop: '5%',
-                marginBottom:'5%',
-                margin: '0 auto',
-                height: '100%'
-            }}>
-                <div style={{
-                    flexDirection: 'column',
-                    flexBasis: '20%',
-                    // border: 'solid pink 1px'
-                }}>
-                    <ul style={{ listStyle: 'none', textAlign: 'left', fontWeight: '500', fontSize: '1.3em', lineHeight: '200%' }}>
-                        <li onClick={(e) => filterCat("Mens")}>Mens</li>
-                        <li onClick={(e) => filterCat("Womens")}>Womens</li>
-                        <li onClick={(e) => filterCat("Pairs")}>Pairs</li>
-                        <li onClick={(e) => filterCat("Ice Dance")}>Ice Dance</li>
+            <div className="DisplayArea">
+
+                <div className="side">
+                    <ul className="theList">
+                        <li className={clicked === "Mens" ? "active" : ""} onClick={(e) => filterCat("Mens")}>Mens</li>
+                        <li className={clicked === "Womens" ? "active" : ""} onClick={(e) => filterCat("Womens")}>Womens</li>
+                        <li className={clicked === "Pairs" ? "active" : ""} onClick={(e) => filterCat("Pairs")}>Pairs</li>
+                        <li className={clicked === "Ice Dance" ? "active" : ""} onClick={(e) => filterCat("Ice Dance")}>Ice Dance</li>
                     </ul>
                 </div>
-                <div style={{
-                    flexDirection: 'column',
-                    flexBasis: '80%',
-                    // border: 'solid pink 1px'
-                }}>
+
+                <div className="SkaterDisplay">
                    <DisplaySkaters skaters={skaterList}/>
- 
                 </div>
+                
             </div>
 
         </motion.div>
